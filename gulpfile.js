53714080edc5f227144817edc0a483524a521c9c
Samuel Vasko <samvasko@gmail.com>
2014-06-06 19:44:15 +0200

    uglify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 
 gulp.task('default', ['browserify'], function () {
     gulp.watch('./src/*.js', ['browserify']);
 });
+
+gulp.task('build', ['browserify', 'uglify']);
 
 gulp.task('browserify', function () {
     var b = browserify('./src/scribe-plugin-image-prompt-command.js');
@@ -15,3 +19,11 @@ gulp.task('browserify', function () {
         .pipe(gulp.dest('.'));
 });
 
+var uglify = require('gulp-uglify');
+
+gulp.task('uglify', function() {
+    gulp.src('./scribe-plugin-image-prompt-command.js')
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('.'))
+});
