4d883b46ad6342e3c447020d1f78f6d2b6a48fff
Samuel Vasko <samvasko@gmail.com>
2014-06-07 18:06:36 +0200

    Allow using custom prompt

--- src/scribe-plugin-image-prompt-command.js
+++ src/scribe-plugin-image-prompt-command.js
@@ -1,10 +1,17 @@
-module.exports = function () {
+/**
+ * Creates simple image. You can pass it a link prompt function
+ * that will be called when command is executed. The function should return
+ * valid url. So please validate.
+ * @param  {function} prompt function
+ * @return {function}
+ */
+module.exports = function (prompt) {
     return function (scribe) {
         var imagePromptCommand = new scribe.api.Command('insertImage');
         imagePromptCommand.nodeName = 'IMG';
 
         imagePromptCommand.execute = function () {
-            link = window.prompt('Gimme link');
+            link = prompt ? prompt() : window.prompt('Enter image url');
             if (link) {
                 if (!/^https?\:\/\//.test(link)) {
                     link = location.protocol + '//' + link;
