cba2c14a8e4b5b0044919ed662e102e729062e5d
Samuel Vasko <samvasko@gmail.com>
2014-06-10 13:42:25 +0200

    Cleanup and added testem

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,28 @@ var browserify = require('browserify');
 var gutil = require('gulp-util');
 var streamify = require('gulp-streamify')
 var rename = require('gulp-rename');
+var glob = require('glob').sync;
 var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 
 gulp.task('default', ['browserify'], function () {
-    gulp.watch('./src/*.js', ['browserify']);
+    gulp.watch(['./src/*.js', 'test/main_test.js'], ['browserify']);
 });
 
 gulp.task('browserify', function () {
     var b = browserify('./' + pkg.main);
-    b.bundle({debug: true})
+    b.bundle({debug: true, standalone: pkg.name})
         .on('error', gutil.log)
         .pipe(source(pkg.name + '.js'))
         .pipe(gulp.dest('.'));
+
+    var t = browserify('./test/main_test.js');
+    t.bundle({debug: true})
+        .on('error', gutil.log)
+        .pipe(source('tests.js'))
+        .pipe(gulp.dest('./test/'));
 });
 
 gulp.task('build', function () {
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+var extend = require('extend');
+
 /**
  * Creates simple image. You can pass it a link prompt function
  * that will be called when command is executed. The function should return
@@ -5,19 +7,26 @@
  * @param  {function} prompt function
  * @return {function}
  */
-module.exports = function (prompt) {
+module.exports = function (options, prompt) {
     return function (scribe) {
         var imagePromptCommand = new scribe.api.Command('insertImage');
         imagePromptCommand.nodeName = 'IMG';
 
         imagePromptCommand.execute = function () {
             link = prompt ? prompt() : window.prompt('Enter image url');
-            if (link) {
-                if (!/^https?\:\/\//.test(link)) {
-                    link = location.protocol + '//' + link;
-                };
-                scribe.api.SimpleCommand.prototype.execute.call(this, link);
+            if (!link) return false;
+            if (typeof link === 'object') {
+                // If some extra properties were passed from prompt
+                options = extend(options, link);
+            } else if (typeof link === 'string') {
+                options.url = link
             };
+
+            if (!/^https?\:\/\//.test(link)) {
+                link = location.protocol + '//' + link;
+            };
+
+            scribe.api.SimpleCommand.prototype.execute.call(this, link);
         }
 
         scribe.commands.imagePrompt = imagePromptCommand;
