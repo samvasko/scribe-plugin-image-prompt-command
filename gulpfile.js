ea6b2e0e0e57497bac51260c6e9eace6c0a105f9
Samuel Vasko <samvasko@gmail.com>
2014-06-13 16:32:15 +0200

    Separated build directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var gutil = require('gulp-util');
 var streamify = require('gulp-streamify')
+var clean = require('gulp-clean');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
+
 var pkg = require('./package.json');
+var target = './build/';
 
 gulp.task('default', ['browserify'], function () {
     gulp.watch(['./src/*.js', 'test/main_test.js'], ['browserify']);
@@ -17,22 +20,26 @@ gulp.task('browserify', function () {
     b.bundle({debug: true, standalone: pkg.name})
         .on('error', gutil.log)
         .pipe(source(pkg.name + '.js'))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest(target));
 
     var t = browserify('./test/main_test.js');
     t.bundle({debug: true})
         .on('error', gutil.log)
         .pipe(source('tests.js'))
-        .pipe(gulp.dest('./test/'));
+        .pipe(gulp.dest(target));
 });
 
-gulp.task('build', function () {
+gulp.task('clean', function () {
+    gulp.src(target, {read: false}).pipe(clean());
+});
+
+gulp.task('build', ['clean'], function () {
     var b = browserify('./' + pkg.main);
     b.bundle({standalone: pkg.name})
         .on('error', gutil.log)
         .pipe(source(pkg.name + '.js'))
-        .pipe(gulp.dest('.'))
+        .pipe(gulp.dest(target))
         .pipe(streamify(uglify()))
         .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('.'))
+        .pipe(gulp.dest(target))
 });
